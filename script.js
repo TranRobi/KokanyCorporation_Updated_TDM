401c4bfebef635026c300b1eec539a9ec42febf4
TranRobi <dtranduy@gmail.com>
2024-06-07 17:48:44 +0200

    fixed search bar

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let totalCost = 0;
 const partlist = document.getElementById("partlist");
 for (let i = 1; i < partlist.rows.length - 1; i++) {
-  totalCost += parseFloat(partlist.rows[i].cells[1].textContent);
+	totalCost += parseFloat(partlist.rows[i].cells[1].textContent);
 }
 document.getElementById("totalcost").textContent = totalCost.toFixed(2);
 
@@ -9,20 +9,20 @@ document.getElementById("totalcost").textContent = totalCost.toFixed(2);
 
 const toc = document.getElementById("toc");
 const headings = document.querySelectorAll(
-  ".main-text > div h1, .main-text > div h2, .comps-box > h3"
+	".main-text > div h1, .main-text > div h2, .comps-box > h3"
 );
 
 console.log(toc);
 console.log(headings);
 
 for (let i = 0; i < headings.length; i++) {
-  headings[i].id = i.toString();
-  const reference = document.createElement("a");
-  reference.textContent = headings[i].textContent;
-  const depth = parseInt(headings[i].tagName[1], 10) - 1; // skip the 'h'
-  reference.href = `#${i}`;
-  if (depth > 0) {
-    reference.classList.add("toc2");
-  }
-  toc.appendChild(reference);
+	headings[i].id = i.toString();
+	const reference = document.createElement("a");
+	reference.textContent = headings[i].textContent;
+	const depth = parseInt(headings[i].tagName[1], 10) - 1; // skip the 'h'
+	reference.href = `#${i}`;
+	if (depth > 0) {
+		reference.classList.add("toc2");
+	}
+	toc.appendChild(reference);
 }
